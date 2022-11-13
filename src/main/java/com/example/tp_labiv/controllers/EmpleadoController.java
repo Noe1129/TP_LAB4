23ff9cd6ad1f22f919b9643293f9775c123335ea
NoeTulian <112957@tecnicatura.frc.utn.edu.ar>
2022-11-12 23:45:09 -0300

    se habilita Cors

--- src/main/java/com/example/tp_labiv/controllers/EmpleadoController.java
+++ src/main/java/com/example/tp_labiv/controllers/EmpleadoController.java
@@ -14,14 +14,17 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @Setter
 @RequestMapping("/empleados")
 public class EmpleadoController {
